facaf5f26c4e0508ab7791821b944c97a2b6e2cd
Jesla <jesslar415@gmail.com>
2016-03-15 16:13:10 -0500

    Early Direction

--- client/js/directives.js
+++ client/js/directives.js
@@ -1,1 +1,13 @@
-angular.module('myApp');
+angular.module('myApp')
+.directive('serverTweets', function() {
+    return { 
+        // this is where you pull the tweets from the server???          
+};
+});
+
+// * create a `$http` request to get all tweets from the backend.
+// * Data will be returned from the service in the correct JSON format, 
+// so you shouldn't have to do any manual processing on your end. You will
+// have access to a `createdAt` property that represents the timestamp of the tweet.
+// ParseObject.CreatedAt
+// * Use `ng-click` on the button in the html to hook up the submit button to the post function.
--- client/js/main.js
+++ client/js/main.js
@@ -7,3 +7,18 @@ angular.module('myApp', [])
 
 }]);
 
+//  ROUTING CODE FROM LECTURE WITH TWEET.HTML ADDED
+// var app = angular.module(‘myApp’, [’ngRoute’]);
+// app.config([‘$routeProvider’, function($routeProvider) {
+// $routeProvider
+// .when(‘/’, {
+// templateUrl: ‘views/welcome.html’,
+// controller: ‘WelcomeController’ **PUT THE CORRECT CONTROLLER NAME HERE
+// })
+// .when(‘/tweets’, {
+// templateUrl: ‘views/tweets.html’,
+// controller: ‘AuctionsController’ **PUT THE CORRECT CONTROLLER NAME HERE
+// })
+// .otherwise({
+// redirectTo: ‘/’
+// });
